a0e8a2435917cb7b04bfe06dbf148b1b65b4bd02
konstrictor <jonkur96@gmail.com>
2020-10-04 22:16:33 +0200

    login template

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
   <div id="app">
+  	<div v-if="logged_in">
+  		
+  	</div>
+  	<login-page v-else/>
   </div>
 </template>
 
 <script>
+import Login from "./pages/Login";
+
 export default {
+	components:{
+		"login-page": Login
+	},
+	data(){
+		return{
+			logged_in: false
+		}
+	}
 };
 </script>
 
-<style>
+<style src="./css/styles.css">
 
 </style>
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,3 @@
 export const routes = [
-	{path: "", component: Home, name:"home"},
+	// {path: "", component: Home, name:"home"},
 ]
