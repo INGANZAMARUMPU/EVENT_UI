273bbff39895cf9012b818476b3a338dee17613b
konstrictor <jonkur96@gmail.com>
2020-10-04 21:34:18 +0200

    empty|initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import {routes} from './routes'
+
+Vue.use(VueRouter);
+const router = new VueRouter({ routes, mode: 'history'})
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
