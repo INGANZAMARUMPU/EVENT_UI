a12db819a3343cd3f5643c651505c1c18e8ede35
konstrictor <jonkur96@gmail.com>
2020-10-05 23:14:41 +0200

    done user cardview

--- src/components/recycler_view.vue
+++ src/components/recycler_view.vue
@@ -6,7 +6,7 @@
         <input type="text" placeholder="recherche..." style="float:right; height:2em">
     </div>
     <div class="recycler">
-        <UserCard/>
+        <UserCard v-for="client in clients" :client="client" :evenmt="evenmt"/>
     </div>
 </div>
 </template>
@@ -19,7 +19,78 @@ export default {
     },
     data(){
         return{
-
+            clients:[
+                {
+                    "id": 5,
+                    "user": {
+                        "first_name": "Tititof",
+                        "last_name": "Irakoze"
+                    },
+                    "fullname": "Tititof Irakoze",
+                    "avatar": "http://127.0.0.1:8000/media/profiles/a327_barbershop_00393_CuDCx3M.jpg",
+                    "phone": "68303893",
+                    "email": "",
+                    "date": "2020-09-29",
+                    "ticket": {
+                        "id": 7,
+                        "name": "VIP",
+                        "price": "20000.0",
+                        "consommable": 5000.0,
+                        "autres": null
+                    },
+                    "autres": "",
+                    "qr": "2ffc431504188360d5f038bd0703359cdad20273a7d62dc0ef4590ad"
+                },
+                {
+                    "id": 3,
+                    "user": {
+                        "first_name": "Jonathan",
+                        "last_name": "Nkurunziza"
+                    },
+                    "fullname": "Jonathan Nkurunziza",
+                    "avatar": "http://127.0.0.1:8000/media/profiles/profile.png",
+                    "phone": "75960696",
+                    "email": "jonkur96@gmail.com",
+                    "date": "2020-09-27",
+                    "ticket": {
+                        "id": 5,
+                        "name": "VIP",
+                        "price": "20000.0",
+                        "consommable": 0.0,
+                        "autres": null
+                    },
+                    "autres": "",
+                    "qr": "11c21fdc598e62d274cd2ba07046e3f955a191e3e9d3e3f115f6958e"
+                },
+                {
+                    "id": 4,
+                    "user": {
+                        "first_name": "Joffray",
+                        "last_name": "Tungamwese"
+                    },
+                    "fullname": "Joffray Tungamwese",
+                    "avatar": null,
+                    "phone": "545667789",
+                    "email": "joffray@gmail.com",
+                    "date": "2020-09-27",
+                    "ticket": {
+                        "id": 6,
+                        "name": "SIMPLE",
+                        "price": "15000.0",
+                        "consommable": 3000.0,
+                        "autres": null
+                    },
+                    "autres": "",
+                    "qr": "e6c744773b1bf5a5eea52d0766181757e6e026a093a54954db64c64d"
+                }
+            ],
+            evenmt:{
+                "id": 1,
+                "name": "Private Party",
+                "place": "GIHOSHA",
+                "date": "2020-10-03",
+                "logo": "http://127.0.0.1:8000/media/logo/images.png"
+            }
         }
     }
 };
--- src/components/user_card.vue
+++ src/components/user_card.vue
@@ -4,22 +4,22 @@
         <img :src='client.avatar' alt="photo de profile" width="30px">
     </div>
     <div class="user-info firstname">
-        {{ client.first_name }}
+        {{ client.user.first_name }}
     </div>
     <div class="user-info lastname">
-        {{ client.last_name }}
+        {{ client.user.last_name }}
     </div>
     <div class="user-info tickettype">
-        {{ client.ticket }}
+        {{ client.ticket.name }}
     </div>
     <div class="hidden-infos">
-        <div class="user-info quarter">{{ evenmt.quarter}}</div>
+        <div class="user-info quarter">{{ client.quarter}}</div>
         <div class="user-info place">{{ evenmt.place}}</div>
         <div class="user-info ticket-date">{{ evenmt.date }}</div>
     </div>
     <div class="user-hover">
         <div class="user-info phone">{{ client.phone}}</div>
-        <div class="user-info email">{{ client.email}}</div>
+        <div class="user-info email">{{ client.user.email}}</div>
         <div class="user-info date">{{ client.date }}</div>
         <a href="#" class="commands-btn btn-print" onclick="generateTicket(event,'{% static 'css/card.css' %}')">
             Imprimer
@@ -37,7 +37,7 @@ export default {
             type:Object,
             required: true
         },
-        event:{
+        evenmt:{
             type:Object,
             required: true
         }
@@ -46,4 +46,60 @@ export default {
 </script>
 
 <style scoped>
+.user-card{
+    display: inline-flex;
+    align-items: center;
+    text-align: center;
+    flex-direction: column;
+    width: 140px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    padding: 5px 5px 10px 5px;
+    margin: 10px;
+    background-color: #fff;
+    border-radius: 5px;
+    white-space: nowrap;
+    position: relative;
+    overflow: hidden;
+}
+.username{
+    max-width: 130px;
+    overflow: hidden;
+}
+.user-card:hover{
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+.hidden-infos{
+    display: none;
+}
+.img-user{
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    background-color: #ddd;
+    overflow: hidden;
+}
+.img-user img{
+    width: 100%;
+    margin:auto;
+}
+.user-hover a{
+    display: block;
+    margin: 3px;
+    padding: 3px;
+    font-size: 16px;
+    font-weight: bold;
+    border: 2px solid #37e;
+    color: #37e;
+    border-radius: 5px;
+}
+.user-hover{
+    width: 130px;
+    height: 100%;
+    background-color: rgba(255,255,255,0.8);
+    position: absolute;
+    display: none;
+}
+.user-card:hover .user-hover{
+    display: block;
+}
 </style>
