e7d54da7208b195efe0e2f2b347567b685f06ca5
konstrictor <jonkur96@gmail.com>
2020-10-07 23:19:30 +0200

    added vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import {routes} from './routes'
+import routes from './routes'
+import store from './store'
 
 Vue.use(VueRouter);
 const router = new VueRouter({ routes, mode: 'history'})
@@ -9,5 +10,6 @@ const router = new VueRouter({ routes, mode: 'history'})
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
